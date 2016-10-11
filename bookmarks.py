321433a43d9c18e1d321312eb5240455b0e30292
Radu Potop <radu@wooptoo.com>
2016-10-11 14:41:03 +0300

    Added samples, fixes

--- bookmarks.py
+++ bookmarks.py
@@ -1,12 +1,23 @@
 import redis
 import hashlib
 import json
+import logging
 from datetime import datetime
+
+logging.basicConfig(level=logging.DEBUG)
 
 # localhost, defaults
 r = redis.StrictRedis(db=0, decode_responses=True)
 
 valid_keys = ['url', 'title', 'tags']
+
+
+_samples = [
+    {'url':'http://kernel.org', 'title':'The Linux Kernel Homepage', 'tags':['linux', 'kernel']},
+    {'url':'http://reddit.com', 'title':'The front page of the internet', 'tags':['reddit', 'social', 'news']},
+    {'url':'http://news.ycombinator.com', 'title':'Hacker News', 'tags':['hackers', 'news', 'social']},
+]
+
 
 class ValidationError(Exception):
     pass
@@ -20,13 +31,13 @@ def validate_entry(entry):
 def _add_tags(entry_tags, entry_hash):
     for tag in entry_tags:
         r.sadd('tag:'+tag, entry_hash)
-    r.sadd('tag_index', entry_tags)
+    r.sadd('tag_index', *entry_tags)
 
 def _update_index(entry_hash):
     r.sadd('index', entry_hash)
 
 def add_entry(entry):
-    entry['date'] = datetime.utcnow()
+    entry['date'] = datetime.utcnow().isoformat()
     entry_hash = hashlib.sha1(entry['url'].encode()).hexdigest()
     entry_body = json.dumps(entry)
 
@@ -44,4 +55,4 @@ def remove_entry(entry_hash):
 
     r.srem('tag_index', entry['tags'])
     r.srem('index', entry_hash)
-    r.del('entry:'+entry_hash)
+    r.delete('entry:'+entry_hash)
